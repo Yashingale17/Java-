a307b71117a3b35715247f538f47cf16052828d5
Yashingale17 <sahilingale329@gmail.com>
2024-08-24 14:01:56 +0530

    syntax

--- Java/Main.java
+++ Java/Main.java
@@ -1,8 +1,19 @@
 
-// public class Main {
-//   public static void main(String[] args) {
-//       System.out.println("Hello, World!");
-//   }
-// }
+
+
+public class Main {
+  public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      System.out.println("Hello Java");
+      // there are difference in println and print statement in this....
+      // for number we do not need to add it in double quotes 
+      System.out.println(7);
+      System.out.println(7 + 7);
+      System.out.println(7 * 7);
+      System.out.println(7 - 6);
+      System.out.print("Hello, World! ");
+      System.out.print("Hello Java");
+  }
+}
 
 // when we write a code in java every thinge should come inside class and class name should match the file name in this case our file name is Main.java os the class name is Main same as file name and main method is used in every class and any thing inside the main methord will run ...
