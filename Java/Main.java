18dc4c681374b03dcfd450f82173ff93ea2d6e73
Yashingale17 <sahilingale329@gmail.com>
2024-08-24 12:05:02 +0530

    java started

---
+++ Java/Main.java
@@ -0,0 +1,8 @@
+
+// public class Main {
+//   public static void main(String[] args) {
+//       System.out.println("Hello, World!");
+//   }
+// }
+
+// when we write a code in java every thinge should come inside class and class name should match the file name in this case our file name is Main.java os the class name is Main same as file name and main method is used in every class and any thing inside the main methord will run ...
